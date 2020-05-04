729b5617b590007939d521266593d43e12d87825
TheDeathlyCow <thedeathlycow@gmail.com>
2020-05-03 22:33:55 -1000

    caramel apple
    added caramel apple, crafted from salt and caramel. gives speed 2 for 10 seconds.
    
    caramel crafted by smelting sugar

--- src/main/java/com/github/thedeathlycow/betterfood/foods/ModFoods.java
+++ src/main/java/com/github/thedeathlycow/betterfood/foods/ModFoods.java
@@ -1,15 +1,19 @@
 package com.github.thedeathlycow.betterfood.foods;
 
 import net.minecraft.item.Food;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 
 public class ModFoods {
 
     public static final Food GREEN_APPLE;
     public static final Food CARAMEL;
+    public static final Food CARAMEL_APPLE;
 
     static {
         GREEN_APPLE = (new Food.Builder()).hunger(4).saturation(0.3F).build();
         CARAMEL = (new Food.Builder()).hunger(1).saturation(0.1f).fastToEat().build();
+        CARAMEL_APPLE = (new Food.Builder()).hunger(4).saturation(0.0f).setAlwaysEdible().effect(new EffectInstance(Effects.SPEED, 200, 1), 100).build();
     }
 
 }
--- src/main/java/com/github/thedeathlycow/betterfood/init/ModItems.java
+++ src/main/java/com/github/thedeathlycow/betterfood/init/ModItems.java
@@ -18,13 +18,15 @@ public class ModItems {
             Setup.setup(new Item(new Item.Properties().group(ItemGroup.FOOD).food(Foods.PUMPKIN_PIE)), "apple_pie"),
             new BlockItem(ModBlocks.SALT_DEPOSIT, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(ModBlocks.SALT_DEPOSIT.getRegistryName()),
             Setup.setup(new Item(new Item.Properties().group(ItemGroup.MATERIALS)), "salt"),
-            Setup.setup(new Item(new Item.Properties().group(ItemGroup.FOOD).food(ModFoods.CARAMEL)), "caramel")
+            Setup.setup(new Item(new Item.Properties().group(ItemGroup.FOOD).food(ModFoods.CARAMEL)), "caramel"),
+            Setup.setup(new Item(new Item.Properties().group(ItemGroup.FOOD).food(ModFoods.CARAMEL_APPLE)), "caramel_apple")
     };
 
     public static final Item GREEN_APPLE = null;
     public static final Item APPLE_PIE = null;
     public static final Item SALT = null;
     public static final Item CARAMEL = null;
+    public static final Item CARAMEL_APPLE = null;
 
 
 }
