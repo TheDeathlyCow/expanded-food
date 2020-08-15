207ab2b707585230cd3376bf7a90cfe51a22c158
TheDeathlyCow <thedeathlycow@gmail.com>
2020-08-14 22:54:46 -1000

    lava berry item texture

--- src/main/java/com/github/thedeathlycow/betterfood/ModEventSubscriber.java
+++ src/main/java/com/github/thedeathlycow/betterfood/ModEventSubscriber.java
@@ -48,6 +48,7 @@ public final class ModEventSubscriber {
             RenderTypeLookup.setRenderLayer(ModBlocks.WILD_RICE_BASE, RenderType.getCutout());
             RenderTypeLookup.setRenderLayer(ModBlocks.WILD_RICE_TOP, RenderType.getCutout());
             RenderTypeLookup.setRenderLayer(ModBlocks.WARPED_YAM, RenderType.getCutout());
+            RenderTypeLookup.setRenderLayer(ModBlocks.LAVA_BERRY_BUSH, RenderType.getCutout());
         }
     }
 
